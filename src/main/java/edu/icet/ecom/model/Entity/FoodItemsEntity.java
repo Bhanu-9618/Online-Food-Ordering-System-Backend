94b3413f2d8f83393b2ee17660b5e8f1ec498354
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-02 10:56:52 +0530

    create Food items and Oreders entities

---
+++ src/main/java/edu/icet/ecom/model/Entity/FoodItemsEntity.java
@@ -0,0 +1,30 @@
+package edu.icet.ecom.model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class FoodItemsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long itemcode;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int packSize;
+
+    @Column(nullable = false)
+    private Double unitPrice;
+
+    @Column(nullable = false)
+    private int qtyOnHand;
+
+}
---
+++ src/main/java/edu/icet/ecom/model/Entity/OrdersEntity.java
@@ -0,0 +1,40 @@
+package edu.icet.ecom.model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class OrdersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long OrderNo;
+
+    @Column(nullable = false , unique = true)
+    private Long orderId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customerEntity;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "item_code")
+    private FoodItemsEntity foodItemsEntity;
+
+    @Column(nullable = false , name = "item_description")
+    private String itemDescription;
+
+    @Column(nullable = false)
+    private Double unitPrice;
+
+    @Column(nullable = false , name = "qty")
+    private int qtyOnHand;
+
+    @Column(nullable = false)
+    private Double total;
+}
