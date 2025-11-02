7db1a55b19af4a6a006a3b930b420f37252939ae
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-02 10:55:55 +0530

    create AddtoCart entity

---
+++ src/main/java/edu/icet/ecom/model/Entity/CustomerEntity.java
@@ -0,0 +1,40 @@
+package edu.icet.ecom.model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long custid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false, unique = true)
+    private String contactno;
+
+    public CustomerEntity(String name, String email, String password, String address, String contactno) {
+        this.address = address;
+        this.contactno = contactno;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
